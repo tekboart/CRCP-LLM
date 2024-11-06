68f52d8d1b8adc0c83216d0d2d53c4224a17d765
tekboart <tekboart@gmail.com>
2024-11-06 18:19:05 +1100

    init Django REST api (1) The POST in (localhost:8000/api/users/create) doesn't work--yet.

---
+++ database_3DCP/api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+#TODO: (ME) I think I should have a class for each node type
+#TODO: (ME) Then use attributes (specific for each node type)
+# FIXME: change "User" to "Node"
+class User(models.Model):
+    """
+    #TODO: Finish class docstring
+    """
+    node_type = models.CharField(max_length=50)
+    parent_node_id = models.IntegerField()
+    # TODO: how to have a list of node features (that are specific to each node_type)
+    # node_features = models.Li
+
+    def __str__(self):
+        return self.node_type
+        # return "???"
+
+# TODO: create an "Edge" class too
+class Edge(models.Model):
+    pass
---
+++ database_3DCP/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import get_users, create_user
+
+
+urlpatterns = [
+    path('users/', get_users, name='get_user'),
+    path('users/create/', create_user, name='create_user')
+]
